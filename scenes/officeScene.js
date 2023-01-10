275942d455b643f57784b752d45f4a822971d290
Klaudia <klaudia.konadu-finke@avanade.com>
2023-01-10 14:40:31 +0100

    camera fix

--- scenes/officeScene.js
+++ scenes/officeScene.js
@@ -6,7 +6,7 @@ var createOfficeScene = function()
 	
 	// camera
 	//var camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI/2, Math.PI / 3, 25, new BABYLON.Vector3(0, 0, 4.5), scene);
-	var camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI/2, 0, 25, new BABYLON.Vector3(-306, -1042, -720), scene); 
+	var camera = new BABYLON.ArcRotateCamera("Camera", 1.57, 0.24, 637.24, new BABYLON.Vector3(0, 0, 0), scene); 
 	//var camera = new BABYLON.WebVRFreeCamera("Camera", new BABYLON.Vector3(0, 1.6, 0), scene);
 	camera.setTarget(BABYLON.Vector3.Zero());
 	camera.attachControl(canvas, true);
@@ -228,7 +228,7 @@ var createOfficeScene = function()
 		        //room scaling and positioning
 				var room = scene.getNodeByName("Collada visual scene group");
 				room.scaling = new BABYLON.Vector3(1, 1, 1);
-				room.position = new BABYLON.Vector3(0, 0, 0);
+				room.position = new BABYLON.Vector3(50, 0, 200);
     };
 
 	
