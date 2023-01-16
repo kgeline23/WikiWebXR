5d96fc91d4babd226d3f85767351fddd95ef2e14
Klaudia <klaudia.konadu-finke@avanade.com>
2023-01-16 14:58:39 +0100

    add teleportation

--- js/navigations.js
+++ js/navigations.js
@@ -1,15 +1,31 @@
-const featuresManager = xr.baseExperience.featuresManager; // or any other way to get a features manager
-featuresManager.enableFeature(WebXRFeatureName.TELEPORTATION, "stable" /* or latest */, {
-  xrInput: xr.input,
-  // add options here
-  floorMeshes: [ground, secondFloor, thirdFloor],
-});
+  var xrHelper = await scene.createDefaultXRExperienceAsync({
+    xrInput: xrHelper.input,
+    floorMeshes: [ground] /* Array of meshes to be used as landing points */,
+  });
+  // ...
+  // needs a reconfigure - re-enable the feature (will discard the old one and create a new one!)
+  let teleportation = xrHelper.teleportation;
+  teleportation = featuresManager.enableFeature(BABYLON.WebXRFeatureName.TELEPORTATION, "stable" /* or latest */, {
+    xrInput: xrHelper.input,
+    floorMeshes: [ground],
+    renderingGroupId: 1,
+  });
 
-const useNavigationPatterns = (xr, floorMeshes) => {
-    const featuresManager = xr.baseExperience.featuresManager; // or any other way to get a features manager
+    // snap points
+  let snapPoint = BABYLON.MeshBuilder.CreateBox('snapPoint', { height: 0.01, width: 1, depth: 1 });
+  snapPoint.position.x = 4;
+  snapPoint.position.z = 3;
+
+  let useNavigationPatterns = (xr, floorMeshes) => {
+    let featuresManager = xr.baseExperience.featuresManager; // or any other way to get a features manager
+    xr.teleportation.addSnapPoint(snapPoint.position);
     featuresManager.enableFeature(BABYLON.WebXRFeatureName.TELEPORTATION, "stable" /* or latest */, {
         xrInput: xr.input,
         // add options here
         floorMeshes: [... floorMeshes],
     });
-}
+  }
+
+
+
+
